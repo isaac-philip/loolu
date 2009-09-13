8c48c4febf6a844000a7e64e486c762fa29fda06
designcurve <designcurve@7fd5ab7c-9f4e-11de-98a9-4d1d5fda2735>
2009-09-13 19:44:58 +0000

    Adding common jquery core, ui and plugins

---
+++ common/jquery/settings.py
@@ -0,0 +1,9 @@
+from ragendja.settings_post import settings
+
+settings.add_app_media('jquery-combined-%(LANGUAGE_CODE)s.js',
+    'jquery/jquery-1.3.2.js',
+    'jquery/jquery-ui-1.7.2.custom.js',
+    'jquery/jquery.cookie.js',
+    'jquery/jquery.parsequery.js',
+)
+
